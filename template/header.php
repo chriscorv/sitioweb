26cfb7305be823a510d9800fd7821155f788ddd2
Christian Salgado <csalgado691@alumnos.cftmaule.cl>
2022-06-22 20:51:58 -0400

    Este es el proyecto

---
+++ template/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Proyecto CFT Maule</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+  </head>
+  <body>
+     <div class="container-fluid">
+        <div class="row">
+            <div class="col-md">
+                <header class="py-3"> 
+                    <h3 class="text-center">Proyecto  CFT Maule</h3>
+                </header>
+            </div>
+        </div>
+     </div>
